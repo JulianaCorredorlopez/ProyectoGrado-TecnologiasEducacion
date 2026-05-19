6715873da389981678390c4bbd1bf5fe566da22b
JulianaCorredorlopez <jandreacorredor@jdc.edu.co>
2026-05-19 08:29:13 -0500

    comen25

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,7 +34,7 @@ export default function Hero() {
                 Iniciar recorrido <Icons.Chevron className="ml-2 h-4 w-4" />
               </Button>
             </a>
-
+            <h1>uhuw</h1>
           </div>
         </div>
 
